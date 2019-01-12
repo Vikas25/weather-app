f595d1d1cad65abee2336d955952fd0c35692aac
Vikas <sanglevikas25@gmail.com>
2019-01-12 21:53:09 +0530

    git

--- weatherapp/src/main/java/com/example/weather/WeatherAppProperties.java
+++ weatherapp/src/main/java/com/example/weather/WeatherAppProperties.java
@@ -1,11 +1,9 @@
 package com.example.weather;
 
-import java.util.Arrays;
-import java.util.List;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("app.weather")
 public class WeatherAppProperties {
@@ -13,29 +11,12 @@ public class WeatherAppProperties {
 	@Valid
 	private final Api api = new Api();
 
-	/**
-	 * Comma-separated list of locations to display. Each entry should have the
-	 * form "Country/City".
-	 */
-	private List<String> locations = Arrays.asList("UK/London", "Russia/Moscow");
-
 	public Api getApi() {
 		return this.api;
 	}
 
-	public List<String> getLocations() {
-		return this.locations;
-	}
-
-	public void setLocations(List<String> locations) {
-		this.locations = locations;
-	}
-
 	public static class Api {
 
-		/**
-		 * API key of the OpenWeatherMap service.
-		 */
 		@NotNull
 		private String key;
 
